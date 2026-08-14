5c2f8866b7b58bc876237955e4cce12cebeca7e4
kothapallira-ops <kothapalli.rani@hcltech.com>
2026-08-14 21:12:46 +0530

    Create EmployeeService.java

---
+++ src/main/java/com/demo/controller/src/main/java/com/demo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    public String getEmployees() {
+        return "Employee List";
+    }
+}
